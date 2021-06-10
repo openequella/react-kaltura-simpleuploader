ad0a8dea014d22539fe4fbf1266322640d841a70
Ian Stevenson <ian.stevenson@edalex.com>
2021-06-10 15:48:17 +1000

    doc: Tidy up TSDoc for index.tsx / render()

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+import { KalturaUploader } from "KalturaUploader";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { KalturaUploader } from "./KalturaUploader";
 
 /**
  * Convenience constant pointing at common endpoint for using Kaltura SaaS platform.
@@ -8,13 +8,16 @@ import { KalturaUploader } from "./KalturaUploader";
 export const kalturaSaasEndpoint = "https://www.kaltura.com/";
 
 /**
- * TODO: Add doco
- * TODO: Add a method to return a link to the new Kaltura entry - maybe a callback
+ * Renders the Kaltura Simple Uploader component to the specified DOM element.
  *
- * @param targetElem
- * @param endpoint
- * @param ks
- * @param partnerId
+ * TODO: Add a method to return a link to the new Kaltura entry - maybe a callback. Need to confirm
+ *       what the openEQUELLA Kaltura plugin is expecting.
+ *
+ * @param targetElem The element to render the component to - typically a `<div>`
+ * @param endpoint The endpoint for the Kaltura server to use. (For Kaltura SaaS simply use
+ *                 `kalturaSaasEndpoint`.
+ * @param ks A Kaltura session token
+ * @param partnerId The partner ID matching the provided `ks`
  */
 export const render = (
   targetElem: HTMLElement,
