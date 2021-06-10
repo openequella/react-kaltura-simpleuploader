5e1c75dc8ede953fca5423f34bc23080166b5a00
Penghai <penghai.zhang@edalex.com>
2021-06-10 11:42:20 +1000

    Specify the module directories for Jest test and add `eslintigore`.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
-// eslint-disable-next-line no-undef
 module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
   preset: "ts-jest",
   testEnvironment: "jsdom",
   testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+  moduleDirectories: [
+    "node_modules",
+    "src"
+  ]
 };
