b1455cc910fbd82b8c56007f9dd47db3a77790a2
Penghai <penghai.zhang@edalex.com>
2021-06-18 15:16:58 +1000

    chore: add a callback for upload in the example project

--- example/index.js
+++ example/index.js
@@ -1,9 +1,16 @@
 import { render, kalturaSaasEndpoint } from "../dist";
 import config from "./config";
 
+const callback = (entries) => {
+  console.log("Callback was called with the following entries:");
+  console.log(entries);
+  document.getElementById("root").textContent = "Process complete, see console log!";
+};
+
 render(
   document.getElementById("root"),
   kalturaSaasEndpoint,
   config.ks,
-  config.partnerId
+  config.partnerId,
+  callback
 );
