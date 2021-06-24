59b8bfadf47c77a2bc5d4b95c9718087cd055505
Ian Stevenson <ian.stevenson@edalex.com>
2021-06-24 10:12:18 +1000

    build: re-enable Storybook reactDocgen
    With the upgrade to SB 6.3 by renovate, this all now works again. Also
    means actions/controls now work again too.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,11 +4,4 @@ module.exports = {
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: ["@storybook/addon-essentials"],
-  // Temporarily disabling while experiencing this issue:
-  // - https://github.com/storybookjs/storybook/issues/15067
-  // - https://github.com/styleguidist/react-docgen-typescript/issues/356
-  // Looks like might be fixed in Storybook 6.3
-  typescript: {
-    reactDocgen: "none",
-  },
 };
